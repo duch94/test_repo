be993fe0ca439a5b210f56b8dd5e16a192088dc7
Victor Titov <vtduch@gmail.com>
2018-10-07 18:42:23 +0300

    my first commit

---
+++ chapter1/today.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>Сегодняшняя дата</title>
+	</head>
+	<body>
+		<p>Сегодняшняя дата (согласно веб-серверу): 
+			<?php
+
+			echo date('l, F jS Y.');
+
+			?>
+		</p>
+	</body>
+</html>
